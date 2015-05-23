c28cd691906042e7fd01b3e5f1c0813b783471b8
Baptiste Fontaine <batifon@yahoo.fr>
2015-05-23 21:59:29 +0200

    brew-p'ypi-resource added': better formatting

--- cmd/brew-pypi-resource.rb
+++ cmd/brew-pypi-resource.rb
@@ -8,12 +8,14 @@ end
 
 pkg, version = *ARGV
 
-url = "https://pypi.python.org/packages/source/#{pkg[0]}/#{pkg}/#{pkg}-#{version}.tar.gz"
-sha256 = `curl -sL #{url} |shasum -a 256`.split(/ +/)[0] # I'm just lazy here
+url = "https://pypi.python.org/packages/source/#{pkg[0]}/#{pkg}/#{pkg.gsub("-", "_")}-#{version}.tar.gz"
+
+# I'm just lazy here. Note that this doesn't check HTTP errors
+sha256 = `curl -sL #{url} |shasum -a 256`.split(/ +/)[0]
 
 puts <<-EOS
-resource "#{pkg}" do
-  url "#{url}"
-  sha256 "#{sha256}"
-end
+  resource "#{pkg}" do
+    url "#{url}"
+    sha256 "#{sha256}"
+  end
 EOS
