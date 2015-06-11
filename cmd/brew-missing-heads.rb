2570bd42b3ad5ddbc40970d0d1b39330e2b659a2
Baptiste Fontaine <batifon@yahoo.fr>
2015-06-12 00:40:57 +0200

    missing-heads: avoid tests

--- cmd/brew-missing-heads.rb
+++ cmd/brew-missing-heads.rb
@@ -3,7 +3,7 @@
 require "formula"
 
 Formula.each do |f|
-  next if f.tap? || f.stable.nil? || f.stable.url !~ /github\.com/ || !f.head.nil?
+  next if f.tap? || f.stable.nil? || f.stable.url !~ /github\.com/ || !f.head.nil? || !f.test_defined?
 
   puts f.full_name
 end
