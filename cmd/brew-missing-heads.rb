0ebf4d9d404186feb52f5c4d7c9152e20e4219f7
Baptiste Fontaine <batifon@yahoo.fr>
2015-06-13 15:12:13 +0200

    unused brew-missing-heads removed

--- cmd/brew-missing-heads.rb
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-require "formula"
-
-Formula.each do |f|
-  next if f.tap? || f.stable.nil? || f.stable.url !~ /github\.com/ || !f.head.nil? || !f.test_defined?
-
-  puts f.full_name
-end
