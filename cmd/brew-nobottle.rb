850a634f0d2a1928849421ff251b40d36767d1c7
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-22 12:07:16 +0200

    brew-nobottle added (wip)

---
+++ cmd/brew-nobottle.rb
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+# 
+# [WIP] Find formulae needing 'bottle :unneeded'
+#
+#
+
+require "formula"
+
+compile = %w[make ./configure gcc cc].map { |cmd| /system "#{cmd}"/ }
+
+Formula.full_names.reject { |name| name.include? "/" }.each do |name|
+  f = Formula[name]
+  next if f.bottle || !f.bottle_specification.collector.keys.empty?
+  next if f.bottle_disabled? || f.bottle_unneeded?
+  next unless f.patchlist.empty? && f.resources.empty?
+
+  # let's do only the simple formulae without deps for now
+  next unless f.deps.empty?
+
+  # extract the 'def install' code
+  install_fn = f.path.read[/  def install\n.*?\n  end/m]
+
+  next if compile.any? { |step| install_fn =~ step }
+
+  puts f.name
+end
