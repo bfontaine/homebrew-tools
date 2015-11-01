e1d9f7f5c4e2ff86bb6b2fbdee2589bea03a0ec3
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-01 01:26:54 +0100

    print the conflicts

--- cmd/brew-bin-conflicts.rb
+++ cmd/brew-bin-conflicts.rb
@@ -6,12 +6,18 @@ tap = Tap.fetch("homebrew", "command-not-found")
 install_tap(tap.user, tap.repo) unless tap.installed?
 
 conflicts = {}
+registered_conflicts = {}
 
 (tap.path/"executables.txt").each_line do |line|
   name, binaries = line.split(":")
   next if binaries.nil?
   next if name.include? "/" # no taps for now
 
+  f = Formula[name]
+
+  next if f.keg_only?
+
+  registered_conflicts[name] ||= f.conflicts.map(&:name)
   binaries.split(" ").each do |bin|
     conflicts[bin] ||= []
     conflicts[bin] << name
@@ -21,23 +27,48 @@ end
 conflicts.select! do |_, c|
   next false if c.size == 1
 
+  # FIXME
   c.all? do |c1|
     cs = c - [c1]
-    (cs & Formula[c1].conflicts.map(&:name)) == cs
+    (cs & registered_conflicts[c1]) == cs
   end
 end
 
-# TODO group conflicts by common names, e.g.:
-#   {
-#     foo1 => bar, qux
-#     foo2 => bar, qux, qux2
-#   }
-#
-#   {
-#     [foo1, foo2] => bar, qux
-#     foo2 => qux2
-#   }
-
+binaries = {}
 conflicts.each do |bin, names|
-  puts "#{bin} is provided by #{names * ", "}"
+  names.each do |name|
+    binaries[name] ||= []
+    binaries[name] << bin
+  end
+end
+
+binaries.each do |name, bins|
+  conflicting_names = {}
+  regconfs = registered_conflicts[name]
+
+  bins.each do |bin|
+    conflicts[bin].delete name
+    conflicts[bin].each do |name2|
+      next if regconfs.include? name2
+
+      conflicting_names[name2] ||= []
+      conflicting_names[name2] << bin
+    end
+  end
+
+  unless conflicting_names.empty?
+    ohai name
+    conflicting_names.each do |n, bs|
+      cause = case bs.size
+              when 1
+                "both install `#{bs[0]}` binaries"
+              when 2, 3, 4
+                "both install #{bs[0..-2].map{|b| "`#{b}`"} * ", "} and `#{bs.last}` binaries"
+              else
+                "both install the same binaries"
+              end
+
+      puts %(conflicts_with "#{n}", :because => "#{cause}")
+    end
+  end
 end
