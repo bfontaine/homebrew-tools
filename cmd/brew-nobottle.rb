26f9eaa2cd04521a8f36e1772e77e460f6fd9e92
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 12:13:52 +0100

    more accurate brew-nobottle

--- cmd/brew-nobottle.rb
+++ cmd/brew-nobottle.rb
@@ -6,10 +6,17 @@
 
 require "formula"
 
-compile = %w[make ./configure gcc cc].map { |cmd| /system "#{cmd}"/ }
+compile_patterns = [
+  /system "rake", "compile"/,
+]
 
-Formula.core_files.each do |name|
-  f = Formula[name]
+%w[make ./configure gcc cc].each { |cmd| compile_patterns << /system "#{cmd}"/ }
+%w[ENV.cc ENV.cxx byte_compile].each { |cmd| compile_patterns << /\b#{cmd}\b/ }
+
+fix = ARGV.include? "--fix"
+
+(ARGV.formulae || Formula.core_files).each do |f|
+  f = Formula[f] unless f.is_a? Formula
   next if f.bottle || !f.bottle_specification.collector.keys.empty?
   next if f.bottle_disabled? || f.bottle_unneeded?
   next unless f.patchlist.empty? && f.resources.empty?
@@ -17,11 +24,21 @@ Formula.core_files.each do |name|
   # let's do only the simple formulae without deps for now
   next unless f.deps.empty?
 
+  content = f.path.read
+
   # extract the 'def install' code
-  install_fn = f.path.read[/  def install\n.*?\n  end/m]
+  install_fn = content[/  def install\n.*?\n  end/m]
 
-  next if compile.any? { |step| install_fn =~ step }
-  next if install_fn.include? "ENV.cc"
+  next if compile_patterns.any? { |p| install_fn =~ p }
 
-  puts f.name
+  ohai f.name
+  next unless fix
+
+  prev_clause = content =~ /^ +head / ? "head" : "sha256"
+  content.gsub!(/^( +)(#{prev_clause} .*\n)/, "\\1\\2\n\\1bottle :unneeded\n")
+  f.path.open("w") { |file| file.write content }
+  FileUtils.cd f.tap.path do
+    system "git", "add", f.path
+    system "git", "commit", "-m", "#{f.name}: bottle is unneeded"
+  end
 end
