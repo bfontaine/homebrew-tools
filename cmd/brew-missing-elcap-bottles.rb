2d9a3fbce83eda6e525a054b25e97cf9a3648cd7
Baptiste Fontaine <batifon@yahoo.fr>
2016-02-24 14:47:07 +0100

    brew-missing-elcap-bottles: support MMOS requirement

--- cmd/brew-missing-elcap-bottles.rb
+++ cmd/brew-missing-elcap-bottles.rb
@@ -5,5 +5,9 @@ require "formula"
 Formula.core_files.each do |file|
   f = Formula[file]
   keys = f.bottle_specification.collector.keys
-  puts f if keys.include?(:yosemite) && !keys.include?(:el_capitan)
+  next if keys.empty? || keys.include?(:el_capitan)
+  next if f.requirements.any? do |r|
+    r.is_a?(MaximumMacOSRequirement) && !r.tags.include?(:el_capitan)
+  end
+  puts f
 end
