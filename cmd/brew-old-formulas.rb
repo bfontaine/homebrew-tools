251f66ff5e27df7c6f439b660e590ce2b243bbee
Baptiste Fontaine <b@ptistefontaine.fr>
2018-10-15 21:37:50 +0200

    Add brew-old-formulas

---
+++ cmd/brew-old-formulas.rb
@@ -0,0 +1,53 @@
+#:  * `old-formulas`:
+#:    Find the top 20 oldest formulae in Homebrew's core tap by last
+#:    modification date.
+
+TOP = 20
+MAX_COMMITS = 15000  # arbitrary
+
+(Tap.new("homebrew", "core").path/"Formula").cd do
+  r, io = IO.pipe
+
+  fork do
+    system("git", "log", "--name-only", "--pretty=format:%aI", "-n #{MAX_COMMITS}",
+           out: io)
+  end
+  io.close
+
+  curr_day = nil
+  last_updates = {}
+
+  r.each_line do |line|
+    if curr_day.nil?
+      curr_day = line.strip.split("T", 2)[0]
+      next
+    end
+
+    if line.strip.empty?
+      curr_day = nil
+      next
+    end
+
+    next unless line.start_with? "Formula/"
+    formula = line.strip.sub "Formula/", ""
+    last_updates[formula] ||= curr_day
+  end
+
+  top = last_updates.sort_by(&:last).slice(0, TOP)
+  max_size = top.map(&:first).map(&:length).max
+  # "YYYY-MM-DD:".length = 11
+  line_format = "%11s %-#{max_size}s"
+
+  prev_day = nil
+
+  top.each do |(formula, day)|
+    if day == prev_day
+      day = ""
+    else
+      prev_day = day
+      day = "#{day}:"
+    end
+
+    puts format(line_format, day, formula)
+  end
+end
